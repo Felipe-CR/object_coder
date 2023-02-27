64776caf171cc99cc56ba4f069342cd1f2db56f0
Felipe <felicampos3942@gmail.com>
2023-02-26 22:31:09 -0500

    Primer Avance Home

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:object_coder/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   static String routeName = "/home";
@@ -7,8 +8,195 @@ class HomeScreen extends StatelessWidget {
     Key? key
   }) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+
+    List catName = [
+      "Categorias",
+      "Clases",
+      "Cursos Gratis",
+      "Libros",
+      "En vivo",
+      "Aprendizaje"
+    ];
+
+    List<Color> catColors = [
+      const Color(0xFFFFCF2F),
+      const Color(0xFF6FE08D),
+      const Color(0xFF61BDFD),
+      const Color(0xFFFC7C7F),
+      const Color(0xFFCB84FB),
+      const Color(0xFF78E667),
+    ];
+
+    List<Icon> catIcons = [
+      const Icon(
+        Icons.category,
+        color: Colors.white,
+        size: 30,
+      ),
+      const Icon(
+        Icons.video_call,
+        color: Colors.white,
+        size: 30,
+      ),
+      const Icon(
+        Icons.assignment,
+        color: Colors.white,
+        size: 30,
+      ),
+      const Icon(
+        Icons.store,
+        color: Colors.white,
+        size: 30,
+      ),
+      const Icon(
+        Icons.play_circle_fill,
+        color: Colors.white,
+        size: 30,
+      ),
+      const Icon(
+        Icons.emoji_events,
+        color: Colors.white,
+        size: 30,
+      ),
+    ];
+
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
+            decoration: const BoxDecoration(
+              color: kPrimaryColor,
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(20),
+                bottomLeft: Radius.circular(20),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Icon(
+                      Icons.dashboard,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                    Icon(
+                      Icons.notifications,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                const Padding(
+                  padding: EdgeInsets.only(left: 3,bottom: 15),
+                  child: Text(
+                    "Hola Programador",
+                    style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1,
+                      wordSpacing: 2,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 10, bottom: 20),
+                  width: MediaQuery.of(context).size.width,
+                  height: 55,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Buscar",
+                      hintStyle: TextStyle(
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                      prefixIcon: const Icon(
+                        Icons.search,
+                        size: 25,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20, right: 15, left: 15),
+            child: Column(
+              children: [
+                GridView.builder(
+                  itemCount: catName.length,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1.1,
+                  ),
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Container(
+                          height: 60,
+                          width: 60,
+                          decoration: BoxDecoration(
+                            color: catColors[index],
+                            shape: BoxShape.circle,
+                          ),
+                          child: Center(
+                            child: catIcons[index],
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          catName[index],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black.withOpacity(0.7),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Cursos",
+                      style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "Más",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: kPrimaryColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
